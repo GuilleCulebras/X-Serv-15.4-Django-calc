9abcb81f60ba94cab86345f80b0a714123418d3f
Guille <guicultal@gmail.com>
2017-03-27 13:03:37 +0200

    Ejercicio resuelto

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def suma(request, num1, num2):
+	suma = int(num1) + int(num2)
+	return HttpResponse(num1 + " + " + num2 + " = " + str(suma))
+
+def resta(request, num1, num2):
+	resta = int(num1) - int(num2)
+	return HttpResponse(num1 + " - " + num2 + " = " + str(resta))
+
+def mult(request, num1, num2):
+	mult = int(num1) * int(num2)
+	return HttpResponse(num1 + " x " + num2 + " = " + str(mult))
+
+def div(request, num1, num2):
+	try:
+		div = int(num1) / int(num2)
+		return HttpResponse(num1 + " / " + num2 + " = " + str(div))
+	except ZeroDivisionError:
+		return HttpResponse("No se puede dividir entre 0")
+
+def error(request):
+	return HttpResponse("404 Not Found")
